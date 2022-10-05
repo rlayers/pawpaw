5405e2a594a3d2230df14bd8f816e0511bbb5c74
rlayers <114594097+rlayers@users.noreply.github.com>
2022-10-05 15:04:05 -0400

    Initial add

---
+++ segments/tests/test_itorator_reflect.py
@@ -0,0 +1,17 @@
+import typing
+
+from segments import Ito
+from segments.itorator import Reflect
+from segments.tests.util import _TestIto
+
+
+class TestItorator(_TestIto):
+    def test_iter(self):
+        s = 'abc'
+        root = Ito(s)
+        self.add_chars_as_children(root, 'Child')
+
+        reflect = Reflect()
+        rv: typing.List[Ito] = [*reflect.traverse(root)]
+        self.assertEqual(1, len(rv))
+        self.assertIs(root, ito)

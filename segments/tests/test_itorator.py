e5688e2fd2df0cc484f7d379e28ba98ccdac59f3
rlayers <114594097+rlayers@users.noreply.github.com>
2022-10-05 15:02:36 -0400

    Changed scope to test Itorator base class

--- segments/tests/test_itorator.py
+++ segments/tests/test_itorator.py
@@ -5,7 +5,8 @@ from segments.itorator import Reflect
 from segments.tests.util import _TestIto
 
 
-class TestReflect(_TestIto):
+class TestItorator(_TestIto):
+    """Uses Reflect class, which has trivial implemntation, to test base class functionality"""
     def test_traverse(self):
         s = 'abc'
         root = Ito(s)
@@ -14,9 +15,10 @@ class TestReflect(_TestIto):
         reflect = Reflect()
         rv: typing.List[Ito] = [*reflect.traverse(root)]
         self.assertEqual(1, len(rv))
-        self.assertIsNot(root, rv[0])
-        self.assertEqual(len(root.children), len(rv[0].children))
-        self.assertEqual(root, rv[0])
+        ito = rv[0]
+        self.assertIsNot(root, ito)
+        self.assertEqual(len(root.children), len(ito.children))
+        self.assertEqual(root, ito)
 
     def test_traverse_with_next(self):
         s = 'abc'
@@ -28,9 +30,10 @@ class TestReflect(_TestIto):
         reflect.itor_next = lambda ito: [ito.clone(descriptor=apply_desc)]
         rv: typing.List[Ito] = [*reflect.traverse(root)]
         self.assertEqual(1, len(rv))
-        self.assertIsNot(root, rv[0])
-        self.assertEqual(len(root.children), len(rv[0].children))
-        self.assertEqual(apply_desc, rv[0].descriptor)
+        ito = rv[0]
+        self.assertIsNot(root, ito)
+        self.assertEqual(len(root.children), len(ito.children))
+        self.assertEqual(apply_desc, ito.descriptor)
 
     def test_traverse_with_children(self):
         s = 'abc'
@@ -41,6 +44,7 @@ class TestReflect(_TestIto):
         reflect.itor_children = lambda ito: (ito.clone(i, i+1, apply_desc) for i, c in enumerate(s))
         rv: typing.List[Ito] = [*reflect.traverse(root)]
         self.assertEqual(1, len(rv))
-        self.assertIsNot(root, rv[0])
-        self.assertEqual(len(s), len(rv[0].children))
-        self.assertTrue(all(c.descriptor == apply_desc for c in rv[0].children))
+        ito = rv[0]
+        self.assertIsNot(root, ito)
+        self.assertEqual(len(s), len(ito.children))
+        self.assertTrue(all(c.descriptor == apply_desc for c in ito.children))

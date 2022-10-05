83e7669afd22a49bbc3f143e51b5d09ac1b4dac1
rlayers <114594097+rlayers@users.noreply.github.com>
2022-10-05 14:26:59 -0400

    Update _version.py

--- segments/_version.py
+++ segments/_version.py
@@ -1,1 +1,22 @@
-__version__ = '2.0.0'
+import typing
+
+
+class PreRelease(typing.NameTuple):
+    kind: str
+    number: int
+
+    def __str__(self) -> str:
+        return f'{self.kind}{self.number}'
+      
+      
+class Version(typing.NameTuple):
+    major: int
+    minor: int
+    micro: int
+    pre_release: PreRelease | None
+
+    def __str__(self) -> str:
+        return f'{self.major}.{self.minor}.{self.micro}{self.pre_release}'
+
+
+__version__ = Version(2, 0, 0, PreRelease('a', 3))

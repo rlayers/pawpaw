f0be2a4e29fce0d0694afd240a12219767512842
rlayers <114594097+rlayers@users.noreply.github.com>
2022-10-05 16:54:36 -0400

    Sync with Itorator._iter changes

--- segments/tests/test_itorator.py
+++ segments/tests/test_itorator.py
@@ -13,7 +13,8 @@ class TestItorator(_TestIto):
         self.add_chars_as_children(root, 'Child')
 
         reflect = Reflect()
-        rv: typing.List[Ito] = [*reflect.traverse(root)]
+        rv = reflect.traverse(root)
+            
         self.assertEqual(1, len(rv))
         ito = rv[0]
         self.assertIsNot(root, ito)
@@ -28,7 +29,8 @@ class TestItorator(_TestIto):
         reflect = Reflect()
         apply_desc = 'x'
         reflect.itor_next = lambda ito: [ito.clone(descriptor=apply_desc)]
-        rv: typing.List[Ito] = [*reflect.traverse(root)]
+        rv = reflect.traverse(root)
+            
         self.assertEqual(1, len(rv))
         ito = rv[0]
         self.assertIsNot(root, ito)
@@ -42,9 +44,25 @@ class TestItorator(_TestIto):
         reflect = Reflect()
         apply_desc = 'x'
         reflect.itor_children = lambda ito: (ito.clone(i, i+1, apply_desc) for i, c in enumerate(s))
-        rv: typing.List[Ito] = [*reflect.traverse(root)]
+        rv = reflect.traverse(root)
+            
         self.assertEqual(1, len(rv))
         ito = rv[0]
         self.assertIsNot(root, ito)
         self.assertEqual(len(s), len(ito.children))
         self.assertTrue(all(c.descriptor == apply_desc for c in ito.children))
+        
+    def test_traverse_with_carry_through(self):
+        s = 'abc'
+        root = Ito(s)
+
+        reflect = Reflect()
+        reflect.itor_children = lambda ito: tuple(ito.clone(i, i+1, 'char') for i in range(*ito.span))
+        reflect.itor_children.itor_next = lambda ito: tuple(ito.clone(descriptor='changed') if i.parent is not None else i for i in [ito])
+        rv = reflect.traverse(root)
+            
+        self.assertEqual(1, len(rv))
+        ito = rv[0]
+        self.assertIsNot(root, ito)
+        self.assertEqual(len(s), len(ito.children))
+        self.assertTrue(all(c.descriptor == 'changed' for c in ito.children))

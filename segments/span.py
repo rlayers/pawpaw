134fc8fa99fd6c3fc735f8cfd73bfcf5e6308d5a
rlayers <114594097+rlayers@users.noreply.github.com>
2022-10-06 10:40:23 -0400

    Create span.py

---
+++ segments/span.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+import collections.abc
+import types
+import typing
+
+from segments.errors import Errors
+
+
+class Span(typing.NamedTuple):
+    start: int
+    stop: int
+        
+    @classmethod
+    def from_indices(
+        cls,
+        basis: int | collections.abc.Sized,
+        start: int | None = None,
+        stop: int | None = None,
+        offset: int = 0
+    ) -> Span:
+        if isinstance(basis, int):
+            length = basis
+        elif isinstance(basis, collections.abc.Sized):
+            length = len(basis)
+        else:
+            raise Errors.parameter_invalid_type('basis', basis, int, collections.abc.Sized)
+
+        if start is None:
+            start = offset
+        elif not isinstance(start, int):
+            raise Errors.parameter_invalid_type('start', start, int, types.NoneType)
+        else:
+            start = min(length, start) if start >= 0 else max(0, length + start)
+            start += offset
+
+        if stop is None:
+            stop = length + offset
+        elif not isinstance(stop, int):
+            raise Errors.parameter_invalid_type('stop', stop, int, types.NoneType)
+        else:
+            stop = min(length, stop) if stop >= 0 else max(0, length + stop)
+            stop += offset
+
+        return Span(start, stop)

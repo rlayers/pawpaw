0432c7178bed24e12ec1960e7ee8878007f4ec09
rlayers <114594097+rlayers@users.noreply.github.com>
2022-10-07 09:54:28 -0400

    Reordering

--- segments/__init__.py
+++ segments/__init__.py
@@ -1,14 +1,14 @@
 from segments._version import __version__
 del _version
 
+from segments.errors import Errors
+del errors
+
 from segments.span import Span
 del span
 
 from segments.ito import Ito, ChildItos
 del ito
-
-from segments.errors import Errors
-del errors
 
 import segments.itorator
 import segments.xml

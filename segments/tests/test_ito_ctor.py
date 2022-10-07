f23f07476427079c67ab00465f67666927012491
rlayers <114594097+rlayers@users.noreply.github.com>
2022-10-07 11:50:34 -0400

    Removed deprecated tests

--- segments/tests/test_ito_ctor.py
+++ segments/tests/test_ito_ctor.py
@@ -22,22 +22,73 @@ class TestItoCtor(_TestIto):
                 self.assertEquals((0, len(s)), i.span)
 
     def test_ctor_str_non_defaults(self):
-        string = 'abcd'
+        s = 'abcd'
         indices = -100, *range(-3, 3), 100
         for start in indices:
             for stop in indices:
                 with self.subTest(bais=s, start=start, stop=stop):
-                    ls = len(s)
-                    start_t = min(ls, start) if start >= 0 else max(0, ls + start)
-                    stop_t = min(ls, stop) if stop >= 0 else max(0, ls + stop)
-                    if start_t > stop_t:
-                        with self.assertRaises(ValueError):
-                            i = Ito(s, start, stop)
-                    else:
-                        i = Ito(s, start, stop)
-                        self.assertLessEqual(0, i.start)
-                        self.assertGreaterEqual(ls, i.stop)
-                        self.assertEqual(s[start:stop], i[:])
+                    i = Ito(s, start, stop)
+                    self.assertLessEqual(0, i.start)
+                    self.assertGreaterEqual(len(s), i.stop)
+                    self.assertEqual(s[start:stop], i[:])
+                    
+    def test_ctor_str_ito(self):
+        for s in '', ' ', 'a', 'abc', ' abc ':
+            for i in None, 0, 1:
+                for j in -1, len(s), None:
+                    basis = Ito(s, i, j)
+                    for start, stop in (None, None), (1, None), (None, -1), (1, -1):
+                        with self.subTest(basis=basis, start=start, stop=stop):
+                            ito = Ito(basis, start, stop)
+                            self.assertEqual(basis.string, ito.string)
+                            self.assertEqual(basis[start:stop], ito[:])
+
+    def test_from_match(self):
+        first = 'John'
+        last = 'Doe'
+        string = ' '.join((first, last))
+        re = regex.compile(r'(?P<fn>.+)\s(?P<ln>.+)')
+        m = re.fullmatch(string)
+
+        desc = 'x'
+        with self.subTest(group='Absent'):
+            ito = Ito.from_match(m, descriptor=desc)
+            self.assertEqual(m.group(), ito.__str__())
+            self.assertEqual(desc, ito.descriptor)
+
+        for group in 0, 1, 'fn', 2, 'ln':
+            with self.subTest(group=group):
+                ito = Ito.from_match(m, group, descriptor=desc)
+                self.assertEqual(m.group(group), ito.__str__())
+                self.assertEqual(desc, ito.descriptor)
+
+    def test_from_substrings(self):
+        string = 'abcd' * 10
+        string = f' {string} '
+
+        with self.subTest(spacing='Consecutive'):
+            for size in ((1, 1), (2, 2), (1, 3), (2, 3)):
+                with self.subTest(size_range=size):
+                    rs = RandSubstrings(size, (0, 0))
+                    subs = [*rs.generate(string, 1, -1)]
+                    itos = Ito.from_substrings(string, *subs)
+                    self.assertListEqual(subs, [i.__str__() for i in itos])
+
+        with self.subTest(spacing='Gaps'):
+            for size in ((1, 1), (2, 2), (1, 3), (2, 3)):
+                with self.subTest(size_range=size):
+                    rs = RandSubstrings(size, (1, 2))
+                    subs = [*rs.generate(string, 1, -1)]
+                    itos = Ito.from_substrings(string, *subs)
+                    self.assertListEqual(subs, [i.__str__() for i in itos])
+
+        with self.subTest(spacing='Overlaps'):
+            for size in ((3, 3),):
+                with self.subTest(size_range=size):
+                    rs = RandSubstrings(size, (-1, -1))
+                    subs = [*rs.generate(string, 1, -1)]
+                    with self.assertRaises(Exception):
+                        itos = [*Ito.from_substrings(string, *subs)]
 
     def test_clone_basic(self):
         string = 'abc'
@@ -90,7 +141,7 @@ class TestItoCtor(_TestIto):
                 else:
                     self.assertEqual(len(parent.children), len(clone.children))
 
-    def test_clone_with_value_funcs(self):
+    def test_clone_with_value_func(self):
         string = ' abcdef '
         f1 = lambda ito: ito.__str__().strip()
         f2 = lambda ito: ito.__str__().upper()
@@ -112,144 +163,3 @@ class TestItoCtor(_TestIto):
         root.value_func = None
         self.assertEqual(string, root.__value__())
         self.assertEqual(f2(clone), clone.__value__())  # Ensure clone didn't change
-
-    def test_offset(self):
-        string = 'abc'
-        d = 'x'
-        ito = Ito(string, 1, 2, descriptor=d)
-
-        params = [
-            {
-                'start': -1,
-            },
-            {
-                'start': -10,
-            },
-            {
-                'start': -1,
-                'stop': 1,
-            },
-            {
-                'start': -20,
-                'stop': 1,
-            },
-            {
-                'stop': 1,
-            },
-            {
-                'stop': 10,
-            },
-            {
-                'start': -1,
-                'stop': 20,
-            },
-            {
-                'start': -1,
-                'stop': 1,
-                'descriptor': 'z'
-            }
-        ]
-
-        for pdict in params:
-            with self.subTest(**pdict):
-                expected = Ito(
-                    string,
-                    ito.start + pdict.get('start', 0),
-                    ito.stop + pdict.get('stop', 0),
-                    pdict.get('descriptor', ito.descriptor)
-                )
-
-                t_start = ito.start + pdict.get('start', 0)
-                t_stop = ito.stop + pdict.get('stop', 0)
-                with warnings.catch_warnings(record=True) as w:
-                    offset = ito.offset(**pdict)
-                    self.assertEqual(expected, offset)
-                if not(0 <= t_start <= len(string) and 0 <= t_stop <= len(string)):
-                    self.assertIsNotNone(w)
-
-    def test_slice(self):
-        string = ' abc '
-        d = 'x'
-        ito = Ito(string, 1, -1, descriptor=d)
-
-        params = [
-            {
-                'start': -1,
-            },
-            {
-                'start': -100,
-            },
-            {
-                'start': -1,
-                'stop': 1,
-            },
-            {
-                'stop': 1,
-            },
-            {
-                'start': -100,
-                'stop': 1,
-            },
-            {
-                'start': -100,
-                'stop': 100,
-            },
-            {
-                'start': -1,
-                'stop': 1,
-                'descriptor': 'z'
-            }
-        ]
-
-        for pdict in params:
-            with self.subTest(**pdict):
-                temp = Ito(ito.__str__(), **pdict)
-                sliced = ito.slice(**pdict)
-                self.assertEqual(temp.__str__(), sliced.__str__())
-
-    def test_from_match(self):
-        first = 'John'
-        last = 'Doe'
-        string = ' '.join((first, last))
-        re = regex.compile(r'(?P<fn>.+)\s(?P<ln>.+)')
-        m = re.fullmatch(string)
-
-        desc = 'x'
-        with self.subTest(group='Absent'):
-            ito = Ito.from_match(m, descriptor=desc)
-            self.assertEqual(m.group(), ito.__str__())
-            self.assertEqual(desc, ito.descriptor)
-
-        for group in 0, 1, 'fn', 2, 'ln':
-            with self.subTest(group=group):
-                ito = Ito.from_match(m, group, descriptor=desc)
-                self.assertEqual(m.group(group), ito.__str__())
-                self.assertEqual(desc, ito.descriptor)
-
-    def test_from_substrings(self):
-        string = 'abcd' * 10
-        string = f' {string} '
-
-        with self.subTest(spacing='Consecutive'):
-            for size in ((1, 1), (2, 2), (1, 3), (2, 3)):
-                with self.subTest(size_range=size):
-                    rs = RandSubstrings(size, (0, 0))
-                    subs = [*rs.generate(string, 1, -1)]
-                    itos = Ito.from_substrings(string, *subs)
-                    self.assertListEqual(subs, [i.__str__() for i in itos])
-
-        with self.subTest(spacing='Gaps'):
-            for size in ((1, 1), (2, 2), (1, 3), (2, 3)):
-                with self.subTest(size_range=size):
-                    rs = RandSubstrings(size, (1, 2))
-                    subs = [*rs.generate(string, 1, -1)]
-                    itos = Ito.from_substrings(string, *subs)
-                    self.assertListEqual(subs, [i.__str__() for i in itos])
-
-        with self.subTest(spacing='Overlaps'):
-            for size in ((3, 3),):
-                with self.subTest(size_range=size):
-                    rs = RandSubstrings(size, (-1, -1))
-                    subs = [*rs.generate(string, 1, -1)]
-                    with self.assertRaises(Exception):
-                        itos = [*Ito.from_substrings(string, *subs)]

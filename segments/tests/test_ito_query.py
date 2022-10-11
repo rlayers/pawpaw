80adb0bb510e9fc0eac680da2862dd5c842f9534
rlayers <114594097+rlayers@users.noreply.github.com>
2022-10-11 15:16:10 -0400

    test improvements

--- segments/tests/test_ito_query.py
+++ segments/tests/test_ito_query.py
@@ -9,89 +9,99 @@ class TestItoQuery(_TestIto):
     def setUp(self) -> None:
         self.src = 'nine 9 ten 10 eleven 11 twelve 12 thirteen 13'
         re = regex.compile(r'(?P<phrase>(?P<word>(?P<char>\w)+) (?P<number>(?P<digit>\d)+)(?: |$))+')
-        self.root = Ito(self.src, desc='root')
+        
+        self.root = Ito(self.src, self.src.index('ten'), self.src.index('thirteen) - 1, desc='root')
         self.root.children.add(*Ito.from_re(re, self.src))
-
-        self.leaf = self.root
-        while len(self.leaf.children) > 0:
-            self.leaf = self.leaf.children[0]
+        self.leaves = [d for d in self.root.walk_descendants() if len(d.children) == 0]
+        self.leaf = self.leaves[0]
+                                                                        
+        self.descs = ['root', 'phrase', 'word', 'char']
 
     # region axis
 
     def test_axis_root(self):
-        i = self.root.find('....')
-        self.assertIs(self.root, i)
-
-        for order in 'n', 'r':
-            with self.subTest(order=order):
-                i = self.leaf.find('....' + order)
-                self.assertEqual(self.root, i)
+        for node_type, node in {'root': self.root, 'leaf': self.leaf}.items():
+            for order in '', 'n', 'r':
+                query = f'....{order}'
+                with self.subTest(node=node_type, query=query):
+                    i = node.find(query)
+                    self.assertEqual(self.root, i)
 
     def test_axis_ancestors(self):
-        expected = [self.leaf.parent]
-        while expected[0].parent is not None:
-            expected.insert(0, expected[0].parent)
-
-        for order in 'n', 'r':
-            with self.subTest(order=order):
-                actual = [*self.leaf.find_all('...' + order)]
-                self.assertListEqual(expected, actual)
-                expected.reverse()
+        for node_type, node in {'root': self.root, 'leaf': self.leaf}.items():
+            for order in '', 'n', 'r':
+                query = f'...{order}'
+                with self.subTest(node=node_type, query=query):
+                    expected = self.descs[:self.descs.index(node.desc)]
+                    if order == 'r':
+                        expected.reverse()
+                    actual = [*node.find_all(query)]
+                    self.assertEqual(len(expected), len(actual))
+                    for e, a in zip(expected, actual)
+                        self.assertEqual(e, a.desc)
 
     def test_axis_parent(self):
-        i = self.root.find('..')
-        self.assertIsNone(i)
-
-        last = self.root
-        while len(last.children) > 0:
-            cur = last.children[0]
-            self.assertEqual(last, cur.find('..'))
-            last = cur
+        for node_type, node in {'root': self.root, 'leaf': self.leaf}.items():
+            for order in '', 'n', 'r':
+                query = f'..{order}'
+                with self.subTest(node=node_type, query=query):
+                    i = node.find(query)
+                    self.assertEqual(node.parent, i)
 
     def test_axis_self(self):
-        for order in 'n', 'r':
-            with self.subTest(order=order):
-                i = self.root.find('.' + order)
-                self.assertIs(self.root, i)
-                i = self.leaf.find('.' + order)
-                self.assertIs(self.leaf, i)
+        for node_type, node in {'root': self.root, 'leaf': self.leaf}.items():
+            for order in '', 'n', 'r':
+                query = f'.{order}'
+                with self.subTest(node=node_type, query=query):
+                    i = node.find(query)
+                    self.assertIs(node, i)
 
+    def test_axis_dedup(self):
+        # First ensure dups present
+        query = f'*/..'
+        rv = [*self.root.find_all(query)]
+        self.assertEqual(len(self.root.children), len(rv))
+        self.assertTrue(all(i is self.root for i in rv))
+                                                                        
+        # Now make sure they get removed
+        for order in '', 'n', 'r':
+            query = f'*/../-{order}'
+            with self.subTest(query=query):
+                rv = [*self.root.find_all(query)]
+                self.assertEqual(1, len(rv))
+                self.assertIs(self.root, rv[0])
+                                                                        
     def test_axis_children(self):
-        order = 'n'
-        with self.subTest(order=order):
-            expected = [*self.root.children]
-            actual = [*self.root.find_all('*' + order)]
-            self.assertListEqual(expected, actual)
-
-        order = 'r'
-        with self.subTest(order=order):
-            expected.reverse()
-            actual = [*self.root.find_all('*' + order)]
-            self.assertListEqual(expected, actual)
+        for node_type, node in {'root': self.root, 'leaf': self.leaf}.items():
+            for order in '', 'n', 'r':
+                query = f'*{order}'
+                with self.subTest(node=node_type, query=query):
+                    expected = [*node.children]
+                    if order == 'r':
+                        expected.reverse()
+                    actual = [*node.find_all(query)]
+                    self.assertListEqual(expected, actual)
 
     def test_axis_descendants(self):
-        order = 'n'
-        with self.subTest(order=order):
-            expected = [*self.root.find_all('**' + order)]
-            self.assertListEqual([*self.root.walk_descendants()], expected)
-
-        order = 'r'
-        with self.subTest(order=order):
-            expected.reverse()
-            actual = [*self.root.find_all('**' + order)]
-            self.assertListEqual(expected, actual)
+        for node_type, node in {'root': self.root, 'leaf': self.leaf}.items():
+            for order in '', 'n', 'r':
+                query = f'**{order}'
+                with self.subTest(node=node_type, query=query):
+                    expected = [*node.walk_descendants()]
+                    if order == 'r':
+                        expected.reverse()
+                    actual = [*node.find_all(query)]
+                    self.assertListEqual(expected, actual)
 
     def test_axis_leaves(self):
-        order = 'n'
-        with self.subTest(order=order):
-            expected = [*(d for d in self.root.walk_descendants() if len(d.children) == 0)]
-            actual = [*self.root.find_all('***' + order)]
-            self.assertListEqual(expected, actual)
-
-        order = 'r'
-        with self.subTest(order=order):
-            expected.reverse()
-            actual = [*self.root.find_all('***' + order)]
-            self.assertListEqual(expected, actual)
+        for node_type, node in {'root': self.root, 'leaf': self.leaf}.items():
+            for order in '', 'n', 'r':
+                query = f'...{order}'
+                with self.subTest(node=node_type, query=query):
+                    expected = self.leaves if node is self.root else []
+                    if order == 'r':
+                        expected.reverse()
+                    actual = [*node.find_all(query)]
+                    self.assertListEqual(expected, actual)
 
     # endregion

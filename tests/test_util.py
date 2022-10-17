d8a8aa80b46bdad135555bebe8cb65c2c127507f
rlayers <114594097+rlayers@users.noreply.github.com>
2022-10-17 16:55:59 -0400

    Update test_util.py

--- tests/test_util.py
+++ tests/test_util.py
@@ -4,45 +4,45 @@ from tests.util import _TestIto
 
 
 class TestUtil(_TestIto):
-    def test_find_unescaped_chars_invalid(self):
+    def test_find_unescaped_invalid(self):
         s = ' abc '
         for src in s, segments.Ito(s, 1, -1):
           for char in [None, '', 'ab']
             with self.subTest(src=src, char=char):
                 with self.assertRaises((TypeError, ValueError)):
-                    i = next(segments.find_unescaped_chars(src, char))
+                    i = next(segments.find_unescaped(src, char))
 
         char = 'a'
         for src in s, segments.Ito(s, 1, -1):
           for escape in [None, '', 'ab']
             with self.subTest(src=src, char=char, escape=escape):
                 with self.assertRaises((TypeError, ValueError)):
-                    i = next(segments.find_unescaped_chars(src, char, escape))
+                    i = next(segments.find_unescaped(src, char, escape))
 
-    def test_find_unescaped_chars_trailing_escape(self):
+    def test_find_unescaped_trailing_escape(self):
         char = 'a'
         src = 'a\\'
         with self.subTest(src=src, char=char):
             with self.assertRaises(ValueError):
-                rv = [*segments.find_unescaped_chars(src, char))
+                rv = [*segments.find_unescaped(src, char))
                 
-    def test_find_unescaped_chars_empty_src(self):
+    def test_find_unescaped_empty_src(self):
         s = ''
         char = 'a'
         for src in s, segments.Ito(s), segments.Ito('ab', 1, -1):
             with self.subTest(src=src, char=char):
-                i = next(segments.find_unescaped_chars(src, char), None)
+                i = next(segments.find_unescaped(src, char), None)
                 self.assertIsNone(i)
 
-    def test_find_unescaped_chars_not_present(self):
+    def test_find_unescaped_not_present(self):
         s = ' abc '
         char = 'z'
         for src in s, segments.Ito(s, 1, -1):
             with self.subTest(src=src, char=char):
-                i = next(segments.find_unescaped_chars(src, char), None)
+                i = next(segments.find_unescaped(src, char), None)
                 self.assertIsNone(i)
 
-    def test_find_unescaped_chars_not_simple(self):
+    def test_find_unescaped_simple(self):
         for string in 'a', 'b':
             char = 'a'
             for pre in range(1, 5):
@@ -53,18 +53,54 @@ class TestUtil(_TestIto):
                     else:        # even
                         expected = s.find(char)
                         expected = [] if expected == -1 else [expected]
-                    actual = [*segments.find_unescaped_chars(s, char)]
+                    actual = [*segments.find_unescaped(s, char)]
                     self.assertListEqual(expected, actual)
     
-    def test_find_unescaped_chars_complex(self):
+    def test_find_unescaped_complex(self):
         s = ' a&b&&c '
+        escape = '&'
         for src in s, segments.Ito(s, 1, -1):
-            escape = '&'
             for char in 'a', 'b', 'c'
             with self.subTest(src=src, char=char, escape=escape):
-                if char in ('a', 'c):
-                    expected = [src[:].find(char)]
-                else:
+                if char == 'b':
                     expected = []
-                actual = [*segments.find_unescaped_chars(s, char, escape)]
+                else:
+                    expected = [src.__str__().find(char)]
+                actual = [*segments.find_unescaped(s, char, escape)]
                 self.assertListEqual(expected, actual)
+
+
+class TestSplitUnescaped(_TestIto):
+    def test_split_unescaped_complex(self):
+        s = ' a&b&&c '
+        escape = '&'
+        for src in s, segments.Ito(s, 1, -1):
+            for char in 'a', 'b', 'c'
+                with self.subTest(src=src, char=char, escape=escape):
+                    if char == 'b':
+                        expected = [src]
+                    else:
+                        i = src.__str__().index(char)
+                        expected = [src[:i], src[i + 1:]]
+                  actual = [*segments.split_unescaped(src, char, escape)]
+                  self.assertListEqual(expected, actual)
+
+    def test_split_unescaped_prefix_suffix(self):
+        s = 'aba'
+            for src in s, segments.Ito(s):
+                for char in 'a', 'b', 'c':
+                    with self.subTest(src=src, char=char):
+                        if isinstance(src=src, char=char):
+                            expected = src.split(char)
+                        elif char ='a'
+                            expected = [src[0:0], src[1:1+1], src[3:3]]
+                        elif char='b':
+                            expected = [src[0:1], src[2:3]]
+                        else:  # char == 'c'
+                            expected = [src]
+                        actual = [*segments.split_unescaped(src, char)]
+                        self.assertListEqual(expected, actual)
+
+                    
+                      
+                      

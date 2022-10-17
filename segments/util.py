5b783edc1cbe9490ff6ad97f5ad99c692fd5c164
rlayers <114594097+rlayers@users.noreply.github.com>
2022-10-17 16:43:41 -0400

    Update util.py

--- segments/util.py
+++ segments/util.py
@@ -5,10 +5,24 @@ import typing
 import segments
 
 
-def find_unescaped_chars(src: str | segments.Types.C, char: str, escape: str = '\\', start: int | None = None, stop: int | None = None) -> typing.Iterable[int]:
-  if not(isinstance(src, str) or isinstance(src, segments.Ito))
+def find_unescaped(
+  src: str | segments.Types.C,
+  char: str,
+  escape: str = '\\',
+  start: int | None = None,
+  stop: int | None = None
+) -> typing.Iterable[int]:
+  if isinstance(src, str):
+      s = src
+      offset = 0
+  elif isinstance(src, segments.Ito)
+      s = src.string
+      offset = src.start
+  else:
       raise segments.Errors.parameter_invalid_type('src', src, str, Ito)
     
+  span = segments.Span.from_indices(src, start, stop).offset(offset)
+  
   if not isinstance(char, str):
       raise segments.Errors.parameter_invalid_type('char', char, str)
   elif len(char) != 1:
@@ -20,13 +34,28 @@ def find_unescaped_chars(src: str | segments.Types.C, char: str, escape: str = '
       raise ValueError('parameter \'escape\' must have length 1')
                        
   esc = False
-  for i, c in enumerate(src[start:stop])
+  for i in range(span.start, span.stop - 1):
+      c = s[i]
       if esc:
           esc = False
       elif c == escape:
           esc = True
       elif c == char:
-          yield i
+          yield i - offset
           
   if esc:
     raise ValueError('parameter \'src\' ends with un-followed escape char \'{escape}\'')
+
+
+def split_unescaped(
+    src: str | segments.Types.C,
+    char: str,
+    escape: str = '\\',
+    start: int | None = None,
+    stop: int | None = None
+) -> typing.Iterable[str] | typing.Iterable[segments.Types.C]:
+    cur = 0
+    for i in find_unescaped(src, char, escape, start, stop):
+        yield src[cur:i]
+        cur = i + 1
+    yield src[cur:]

ea293b9bda85238a02c68d69313e60b408aea07e
rlayers <114594097+rlayers@users.noreply.github.com>
2022-10-17 15:18:21 -0400

    Update test_span.py

--- tests/test_span.py
+++ tests/test_span.py
@@ -1,20 +1,26 @@
-from segments import Span
+from segments import Span, Ito
 from tests.util import _TestIto
 
 
 class TestSpan(_TestIto):
     def test_from_indices_valid(self):
-        for basis in ['', ' ', ' abc ']:
-            with self.subTest(basis=basis):
-                for start in (-100, -1, None, 0, 1, 100):
-                    with self.subTest(start=start):
-                        for stop in (-100, -1, None, 0, 1, 100):
-                            with self.subTest(stop=stop):
-                                _slice = slice(start, stop)
-                                expected = basis[_slice]
-                                span = Span.from_indices(basis, start, stop)
-                                self.assertEqual(expected, basis[span.start:span.stop])
-                                self.assertEqual(expected, basis[slice(*span)])
+        for s in ['', ' ', ' abc ']:
+            for start in (-100, -1, None, 0, 1, 100):
+                for stop in (-100, -1, None, 0, 1, 100):
+                    if len(s) == 0:
+                        ito = Ito(s)
+                    elif len(s) == 1:
+                        ito = Ito(s, 1)
+                    else:
+                        ito = Ito(s, 1, -1)
+
+                    for basis in s, ito:
+                        with self.subTest(basis=basis, start=start, stop=stop):
+                            _slice = slice(start, stop)
+                            expected = basis[_slice]
+                            span = Span.from_indices(basis, start, stop)
+                            actual = basis[slice(*span)]
+                            self.assertEqual(expected, actual)
 
     def test_from_indices_invalid_base(self):
         for basis in [None, 1.0]:
@@ -28,3 +34,17 @@ class TestSpan(_TestIto):
             with self.subTest(basis=s, **{k: v}):
                 with self.assertRaises(TypeError):
                     Span.from_indices(s, **{k: v})
+                    
+    def test_offset(self):
+        s = 'abc'
+        for basis in s, Ito(s, 1, -1)
+            for i in (-100, -1, None, 0, 1, 100):
+                with self.subTest(basis=basis, i=i):
+                    span = Span.from_indices(basis)
+                    if (span.start + i < 0) or (span.stop + i < 0):
+                        with self.assertRaises(ValueError):
+                            span.offset(i)
+                    else:
+                        rv = span.offset(i)
+                        self.assertEqual(span.start + i, rv.start)
+                        self.assertEqual(span.stop + 1, rv.stop)

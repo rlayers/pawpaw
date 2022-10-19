d62350e15947985267105c318345ba33a1a7aaf0
rlayers <114594097+rlayers@users.noreply.github.com>
2022-10-19 14:10:02 -0400

    Split funcs to TypeChecking

--- segments/errors.py
+++ segments/errors.py
@@ -1,5 +1,4 @@
 from __future__ import annotations
-import inspect
 import typing
 
 
@@ -16,18 +15,3 @@ class Errors:
     def parameter_invalid_type(cls, name: str, value: typing.Any, *allowed: typing.Type) -> TypeError:
         types = ' or '.join(t.__qualname__ for t in allowed)
         return TypeError(f'parameter \'{name}\' must be type {types}, not {type(value).__qualname__}')
-
-    @classmethod
-    def is_callable(cls, val: typing.Any, *params: typing.Type) -> bool:
-        if not isinstance(val, typing.Callable):
-            return False
-
-        ips = inspect.signature(val).parameters
-        if len(params) != len(ips):
-            return False
-
-        for ipv, p in zip(ips.values(), params):
-            if ipv.annotation != inspect._empty and p != ipv.annotation:  # TODO : when p is singular type and annotation is Union
-                return False
-
-        return True

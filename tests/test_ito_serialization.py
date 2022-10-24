a9b3e743c2d08d26d89ba79eaa5e2e93f879d977
rlayers <114594097+rlayers@users.noreply.github.com>
2022-10-24 17:14:06 -0400

    Added json_stringless tests

--- tests/test_ito_serialization.py
+++ tests/test_ito_serialization.py
@@ -32,9 +32,33 @@ class TestItoSerialization(_TestIto):
         js_data = json.dumps(word, cls=Ito.JsonEncoder)
         expected_prefix = '{"__type__": "typing.Tuple[str, Ito]", "string": "' + \
             word.string + \
-            '", "ito": {"__type__": "Ito", "_span": ' + \
+            '", "ito": {"__type__": "Ito", "span": ' + \
             str(list(word.span)) + \
             ', "desc": "' + \
             word.desc + \
             '"'
         self.assertTrue(js_data.startswith(expected_prefix))      
+
+    def test_json_deserialize(self):
+        word = self.h_ito.find('**[d:Word]')
+        js_data = json.dumps(word, cls=Ito.JsonEncoder)
+        w_deser = json.loads(js_data, object_hook=Ito.json_decoder)
+        self.assertEqual(w_orig, w_deser)
+        
+    def test_json_stringless_serialize(self):
+        word = self.h_ito.find('**[d:Word]')
+        js_data = json.dumps(word, cls=Ito.JsonEncoderStringless)
+        expected_prefix = '{"__type__": "Ito", "span": ' + \
+            str(list(word.span)) + \
+            ', "desc": "' + \
+            word.desc + \
+            '"'
+        self.assertTrue(js_data.startswith(expected_prefix))      
+
+    def test_json_stringless_deserialize(self):
+        word = self.h_ito.find('**[d:Word]')
+        js_data = json.dumps(word, cls=Ito.JsonEncoderStringless)
+        w_deser = json.loads(js_data, object_hook=Ito.json_decoder_stringless)
+        self.assertNotEqual(w_orig, w_deser)
+        w_deser._set_string(w_orig.string)
+        self.assertListEqual([w_orig, *w_orig.children], [w_deser, *w_deser.children])

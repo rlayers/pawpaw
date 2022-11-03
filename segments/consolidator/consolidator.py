3c1a3fb2bfa27cce1b8f42b9ac93521b8023e447
rlayers <114594097+rlayers@users.noreply.github.com>
2022-11-03 14:42:50 -0400

    Update consolidator.py

--- segments/consolidator/consolidator.py
+++ segments/consolidator/consolidator.py
@@ -14,6 +14,51 @@ from segments.errors import Errors
 #
 #   parent.children.add(*outgoing)
 
+
+class ConsolidatorEx(ABC):
+    @abstractmethod
+    def traverse(self, itos: Types.C_IT_ITOS) -> Types.C_IT_BITOS:
+        ...
+        
+        
+class WindowedJoinEx(Consolidator):
+    F_SQ_ITOS_2_B = typing.Callable[[Types.C_SQ_ITOS], bool]
+    
+    def __init__(self, window_size: int, predicate: F_SQ_ITOS_2_B, desc: str | None = None):
+        super().__init__()
+        
+        if not isinstance(window_size, int):
+            raise Errors.parameter_invalid_type('window_size', window_size, int)
+        if window_size < 2:
+            raise ValueError(f'parameter \'window_size\' has value {window_size:,}, but must be greater than or equal to 2')
+        self.window_size = window_size
+
+        if not Types.is_callable(predicate, self.F_SQ_ITOS_2_B):
+            raise Errors.parameter_invalid_type('predicate', predicate, self.F_SQ_ITOS_2_B)
+        self.predicate = predicate
+
+        self.desc = desc
+
+    def traverse(self, itos: Types.C_IT_ITOS) -> Types.C_IT_BITOS:
+        joined: Types.C_ITO | None = None
+        window: typing.List[Types.C_ITO] = []
+        for ito in itos:
+            window.append(ito)
+            if len(window) == self.window_size:  # Only when window is full?
+                if self.predicate(window):
+                    cur = ito.join(window, desc=self.desc)
+                    if window[0] is joined:
+                        del window[0]
+                    yield from (Types.C_BITO(False, i) for i in window)
+                    window.clear()
+                    window.append(cur)
+                    joined = cur
+                else:
+                    yield Types.C_BITO(False, window.pop(0))
+
+        yield from (Types.C_BITO(False, i) for i in window)
+
+        
 class Consolidator(ABC):
     @abstractmethod
     def traverse(self, itos: Types.C_IT_ITOS) -> Types.C_IT_ITOS:

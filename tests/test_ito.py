9e75a0b2c52eda7e0a7ebb7a5307725b41483d4a
rlayers <114594097+rlayers@users.noreply.github.com>
2022-11-04 17:44:39 -0400

    Added .test_reclaim & .test_invert_children

--- tests/test_ito.py
+++ tests/test_ito.py
@@ -278,6 +278,27 @@ class TestIto(_TestIto):
         self.assertTrue(all(joined is not child for child in children))
         self.assertEqual(s, str(joined))
         self.assertEqual(joined_desc, joined.desc)
+
+    def test_reclaim(self):
+        # TODO : Write test
+        self.assertTrue(False)
+
+    def test_invert_children(self):
+        for s in '', 'AB', ' AB', 'A B', 'AB ', ' A B', 'A B ', ' A B ':
+            parent = Ito(s)
+            with self.subTest(string=s, children_added=False):
+                inverted = parent.invert_children()
+                expected = ''.join(str(c) for c in inverted.children)
+                self.assertEqual(s, expected)
+
+            parent.children.add(*parent.str_split())
+            with self.subTest(string=s, children_added=True):
+                inverted = parent.invert_children()
+                clone = parent.clone(clone_children=False)
+                clone.children.add(*(c.lone() for c in parent.children))
+                clone.children.add(*(c.lone() for c in inverted.children))
+                expected = ''.join(str(c) for c in inverted.children)
+                self.assertEqual(s, expected)
         
     def test_split_iter_simple(self):
         basis = 'A B C D E'

d58d87d4e5cd6bc9eef637d76bf04951d712fb31
rlayers <114594097+rlayers@users.noreply.github.com>
2022-11-23 11:23:56 -0500

    added .find_escapes

--- pawpaw/util.py
+++ pawpaw/util.py
@@ -4,12 +4,45 @@ import typing
 import pawpaw
 
 
+# Finds indeces of non-doubled escape chars
+def find_escapes(
+    src: str | pawpaw.Types.C_ITO,
+    escape: str = '\\',
+    start: int | None = None,
+    stop: int | None = None
+) -> typing.Iterable[int]:
+    if isinstance(src, str):
+        s = src
+        offset = 0
+    elif isinstance(src, pawpaw.Ito):
+        s = src.string
+        offset = src.start
+    else:
+        raise pawpaw.Errors.parameter_invalid_type('src', src, str, pawpaw.Ito)
+
+    span = pawpaw.Span.from_indices(src, start, stop).offset(offset)
+
+    if not isinstance(escape, str):
+        raise pawpaw.Errors.parameter_invalid_type('escape', escape, str)
+    elif len(escape) != 1:
+        raise ValueError('parameter \'escape\' must have length 1')
+
+    esc = False
+    for i in range(span.start, span.stop):
+        c = s[i]
+        if c == escape:
+            esc = not esc
+        elif esc:
+            yield i - 1
+            esc = False
+
+
 def find_unescaped(
-        src: str | pawpaw.Types.C_ITO,
-        char: str,
-        escape: str = '\\',
-        start: int | None = None,
-        stop: int | None = None
+    src: str | pawpaw.Types.C_ITO,
+    char: str,
+    escape: str = '\\',
+    start: int | None = None,
+    stop: int | None = None
 ) -> typing.Iterable[int]:
     if isinstance(src, str):
         s = src
@@ -47,11 +80,11 @@ def find_unescaped(
 
 
 def split_unescaped(
-        src: str | pawpaw.Types.C_ITO,
-        char: str,
-        escape: str = '\\',
-        start: int | None = None,
-        stop: int | None = None
+    src: str | pawpaw.Types.C_ITO,
+    char: str,
+    escape: str = '\\',
+    start: int | None = None,
+    stop: int | None = None
 ) -> typing.Iterable[str] | typing.Iterable[pawpaw.Types.C_ITO]:
     cur = 0
     for i in find_unescaped(src, char, escape, start, stop):

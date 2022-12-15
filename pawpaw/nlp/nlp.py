a9ccfd09a2afec2cbd5d380dc598c73095905e0d
rlayers <114594097+rlayers@users.noreply.github.com>
2022-12-15 12:41:31 -0500

    Table stub

--- pawpaw/nlp/nlp.py
+++ pawpaw/nlp/nlp.py
@@ -172,6 +172,47 @@ class Number:
         return self._re
 
 
+class Table:
+    """
+    Style 1: md style
+
+    | XXX | YYY |
+    | --- | --- |
+    | a1a | b1b |
+    | a2a | b2b |
+
+    Style 2: rst simple
+
+    =====  =====  ======
+       Inputs     Output
+    ------------  ------
+    A      B    A or B
+    =====  =====  ======
+    False  False  False
+    True   False  True
+    False  True   True
+    True   True   True
+    =====  =====  ======
+
+    Style 3: rst Grid
+
+    +------------+------------+-----------+
+    | Header 1   | Header 2   | Header 3  |
+    +============+============+===========+
+    | body row 1 | column 2   | column 3  |
+    +------------+------------+-----------+
+    | body row 2 | Cells may span columns.|
+    +------------+------------+-----------+
+    | body row 3 | Cells may  | - Cells   |
+    +------------+ span rows. | - contain |
+    | body row 4 |            | - blocks. |
+    +------------+------------+-----------+
+
+    """
+
+    pass
+
+
 class List:
     """
     quantifier patterns

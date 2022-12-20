7a0e256e2fc05a964480d053da352cd8f869446a
rlayers <a.nov.guy@gmail.com>
2022-12-20 18:21:53 -0500

    Xml Doc edits

--- tinker.py
+++ tinker.py
@@ -14,6 +14,7 @@ import xml.etree.ElementTree as ET
 from pawpaw import xml
 text = """<?xml version="1.0"?>
 <music xmlns:mb="http://musicbrainz.org/ns/mmd-1.0#" xmlns="http://mymusic.org/xml/">
+    <?display table-view?>
     <album genre="R&amp;B" mb:id="123-456-789-0">
         Robson Jorge &amp; Lincoln Olivetti <!-- 1982, Vinyl -->
     </album>

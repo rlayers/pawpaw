1e9182723430358fdecb96d2db7d6c03db0701da
rlayers <114594097+rlayers@users.noreply.github.com>
2023-01-10 10:54:55 -0500

    Added test_corner_combos

--- tests/test_visualization_ascii_box.py
+++ tests/test_visualization_ascii_box.py
@@ -26,11 +26,29 @@ class TestAsciiBoxDrawing(_TestIto):
         else:
             return False
 
-    def test_from_corners_single(self):
+    def test_from_corners_single_valid(self):
         for bdc in box.BoxDrawingChar.from_char('╭',),:  # box.BoxDrawingChar._instances:
             with self.subTest(box_drawing_char=bdc):
                 if self.is_corner(bdc):
                     boxer = box.from_corners(bdc)
                 else:
                     with self.assertRaises(ValueError):
-                        boxer = box.from_corners(bdc)                   
+                        boxer = box.from_corners(bdc)
+
+    def test_corner_combos(self):
+        in_outs = (
+            (('╔', '╯'), ('╔', '╕', '╙', '╯')),
+            (('╚', '╮'), ('╓', '╮', '╚', '╛'))
+        )
+
+        for ins, output_corners in in_outs:
+            for input_corners in ins, ins[::-1]:
+                with self.subTest(input_corners=input_corners):
+                    input_corners = [box.BoxDrawingChar.from_char(c) for c in input_corners]
+                    boxer = box.from_corners(*input_corners)
+                    lines = list(boxer.from_srcs(' '))
+                    self.assertEqual(3, len(lines))
+                    self.assertEqual(output_corners[0], lines[0][0])
+                    self.assertEqual(output_corners[1], lines[0][-1])
+                    self.assertEqual(output_corners[2], lines[-1][0])
+                    self.assertEqual(output_corners[3], lines[-1][-1])

c91e57520b837acb5a20e236cc8a62aec68dcdb5
rlayers <114594097+rlayers@users.noreply.github.com>
2023-01-25 18:21:02 -0500

    Furcation re-architecture

--- pawpaw/arborform/furcation.py
+++ pawpaw/arborform/furcation.py
@@ -5,58 +5,22 @@ import types
 
 from pawpaw import Ito, Types, Errors, arborform
 
+F_PREDICATE = typing.Callable[[typing.Any], bool]
 
-T = typing.TypeVar('T', arborform.Itorator, arborform.Postorator)
+class PredicatedValue:
+    def __init__(self, predicate: F_PREDICATE, val: typing.Any):
+        if not Types.is_callable(predicate, F_PREDICATE):
+            raise Errors.parameter_invalid_type('predicate', predicate, F_PREDICATE)
+        self._predicate: F_PREDICATE = predicate
+        self._val: typing.Any = val
 
+        @property
+        def predicate(self) -> F_PREDICATE:
+            return self._predicate
 
-@dataclasses.dataclass
-class PredicatedValue(typing.Generic[T]):
-    predicate: Types.F_ITO_2_B
-    val: T | None
-
-    def __post_init__(self):
-        if not Types.is_callable(self.predicate, Types.F_ITO_2_B):
-            raise Errors.parameter_invalid_type('predicate', self.predicate, Types.F_ITO_2_B)
-
-        if not isinstance(self.val, (arborform.Itorator, arborform.Postorator)):
-            raise Errors.parameter_invalid_type('val', self.val, arborform.Itorator, arborform.Postorator)
-
-
-class Furcation(list[PredicatedValue[T]]):
-    def evaluate(self, ito: Ito) -> T | None:
-        for pv in self:
-            if pv.predicate(ito):
-                return pv.val
-
-        return None
-
-    def append(self, item1: PredicatedValue[T] | Types.F_ITO_2_B | T, item2: T = None) -> None:
-        # Prefer PredicatedVal[T], but can't use subscripted generics for class and instance checks
-        if isinstance(item1, PredicatedValue):
-            item = item1
-        elif Types.is_callable(item1, Types.F_ITO_2_B):
-            if isinstance(item2, (arborform.Itorator, arborform.Postorator)) or item2 is None:
-                item = PredicatedValue(item1, item2)
-            else:
-                raise Errors.parameter_invalid_type('item2', item2, arborform.Itorator, arborform.Postorator, None)
-        # Prefer isinstance(item1, T), but this is not currently supported in Python
-        elif isinstance(item1, (arborform.Itorator, arborform.Postorator)):
-            item = PredicatedValue(lambda ito: True, item2)
-        else:
-            raise Errors.parameter_invalid_type('item1', item1, PredicatedValue[T], Types.F_ITO_2_B, arborform.Itorator, arborform.Postorator)
-
-        super().append(item)
-
-
-it_root = arborform.Itorator.wrap(lambda ito: ito, tag='root')
-it1 = arborform.Itorator.wrap(lambda ito: ito.str_ltrim(), tag='it1')
-it2 = arborform.Itorator.wrap(lambda ito: ito.str_rtrim(), tag='it2')
-it3 = arborform.Itorator.wrap(lambda ito: ito.str_split(), tag='it3')
-
-        
-ito_reg = Ito(' a b c ')
-ito_ws = Ito('\t\t  ')
-ito_empty = Ito('')
+        @property
+        def val(self) -> typing.Any:
+            return self._val
 
 #########
 # TESTS #
@@ -68,11 +32,54 @@ def ValidPredicate(ito: Ito) -> bool:
 def InvalidPredicate(ito: Ito) -> str:
     return 'abc'
 
-foo = PredicatedValue[arborform.Itorator](InvalidPredicate, it1)  # ValueError for predicate
+it_root = arborform.Itorator.wrap(lambda ito: ito, tag='root')
+it1 = arborform.Itorator.wrap(lambda ito: ito.str_ltrim(), tag='it1')
+it2 = arborform.Itorator.wrap(lambda ito: ito.str_rtrim(), tag='it2')
+it3 = arborform.Itorator.wrap(lambda ito: ito.str_split(), tag='it3')
+        
+ito_reg = Ito(' a b c ')
+ito_ws = Ito('\t\t  ')
+ito_empty = Ito('')
 
-foo = PredicatedValue[arborform.Itorator](ValidPredicate, 'abc')  # ValueError for 'val'
+pv = PredicatedValue(lambda i: -1, 'Yes!')  # Ok: Invalid lambda rv, but no types available on lambdas so passes
 
-foo = PredicatedValue[arborform.Itorator](lambda ito: False, 'abc')  # ValueError for 'val'
+pv = PredicatedValue(lambda i, j: True, 'Yes!')  # Invalid lambda param count
 
-foo = PredicatedValue[arborform.Itorator](ValidPredicate, it1)  # OK
-foo = PredicatedValue[arborform.Itorator](lambda ito: False, it1)  # OK
+pv = PredicatedValue(InvalidPredicate, it1)  # Invalid predicate
+
+pv = PredicatedValue(ValidPredicate, it1)  # OK
+
+pv = PredicatedValue(lambda ito: False, it1)  # OK
+
+P = typing.TypeVar('P')  # Input to predicate
+R = typing.TypeVar('R')  # Return value type; should be "anything but None", but Python lacks this ability
+
+class Furcation(list[PredicatedValue], typing.Generic[P, R]):
+    def evaluate(self, ito: Ito) -> R | None:
+        for pv in self:
+            if pv.predicate(ito):
+                return pv.val
+
+        return None
+
+    def append(self, item1: PredicatedValue | typing.Callable[[P], bool] | R, item2: R | None = None) -> None:
+        p, r = typing.get_args(self.__orig_class__)
+
+        if isinstance(item1, PredicatedValue):
+            item = item1
+        elif isinstance(item1, typing.Callable):
+            if Types.is_callable(item1, typing.Callable[[p], bool]):
+                if isinstance(item2, r) or item2 is None:
+                    item = PredicatedValue(item1, item2)
+                else:
+                    raise Errors.parameter_invalid_type('item2', item2, *typing.get_args(r | None))
+            else:
+                raise Errors.parameter_invalid_type('item1', item1, PredicatedValue, typing.Callable[[p], bool], r)
+        elif Types.type_matches_annotation(type(item1), r):
+            item = PredicatedValue(lambda ito: True, item2)
+        else:
+            raise Errors.parameter_invalid_type('item1', item1, PredicatedValue, typing.Callable[[p], bool], r)
+
+        super().append(item)
+
+arc_list = Furcation[Ito, arborform.Itorator]()

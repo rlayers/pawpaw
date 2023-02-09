f72362582a5505a95308c66a2eafcf4065ac7290
rlayers <114594097+rlayers@users.noreply.github.com>
2023-02-09 16:22:32 -0500

    Initital add

---
+++ pawpaw/arborform/itorator/value_func.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+from pawpaw import Ito, Types, Errors, type_magic
+from pawpaw.arborform.itorator import Itorator
+
+
+class ValueFunc(Itorator):
+    def __init__(self, f: Types.F_ITO_2_VAL | None, tag: str | None = None):
+        super().__init__(tag)
+        if not (f is None or type_magic.functoid_isinstance(f, Types.F_ITO_2_VAL)):
+            raise Errors.parameter_invalid_type('f', f, Types.F_ITO_2_VAL, None)
+        self.f = f
+
+    def _iter(self, ito: Ito) -> Types.C_SQ_ITOS:
+        ito.value_func = f
+        ito.desc = self._desc_func(ito)
+        return ito,

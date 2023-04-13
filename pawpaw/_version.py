0a717f9808a5d590dff6b021cfa48f3585ea8651
rlayers <114594097+rlayers@users.noreply.github.com>
2023-04-13 16:49:13 -0400

    _Version refactoring to Pep-440

--- pawpaw/_version.py
+++ pawpaw/_version.py
@@ -1,41 +1,49 @@
 from __future__ import annotations
 
+import string
 import typing
 
-__version__ = '1.0.0.0'
-"""The str literal that build, setup, documentation, and other tools typically want
+__version__ = '1.0.0.rc1'
+"""The str literal that build, setup, documentation, and other tools typically want.
+See https://peps.python.org/pep-0440/ for recommended module versioning schema.
 """
 
-
-class _PreRelease(typing.NamedTuple):
-    kind: str  # 'a', 'b', or 'rc' : see https://peps.python.org/pep-0440/
-    number: int
+class _Segment(typing.NamedTuple):
+    value: int
+    decorator: str | None
 
     def __str__(self) -> str:
-        return f'{self.kind}{self.number}'
-      
-      
+        return f'{self.value}'
+
+class _PrefixedSegment(_Segment):
+    def __str__(self) -> str:
+        return self.decorator + super().__str__()
+
+class _SuffixedSegment(_Segment):
+    def __str__(self) -> str:
+        return super().__str__() + self.decorator
+
+def to_segment(segment: str) -> _Segment:
+        if segment.isnumeric():
+            return _Segment(int(segment), None)
+
+        lstrp = segment.lstrip(string.ascii_lowercase)
+        if (d := len(segment) - len(lstrp)) != 0:
+            return _PrefixedSegment(int(lstrp), segment[:d])
+
+        rstrp = segment.rstrip(string.ascii_lowercase)
+        d = len(segment) - len(rstrp)
+        return _SuffixedSegment(int(rstrp), segment[-d:])
+
 class _Version(typing.NamedTuple):
-    major: int
-    minor: int
-    micro: int
-    pre_release: _PreRelease | None
-
-    def __str__(self) -> str:
-        return f'{self.major}.{self.minor}.{self.micro}{self.pre_release}'
+    segments: tuple[_Segment]
 
     @classmethod
     def from_(cls, version: str) -> _Version:
-        parts = version.split('.')
-        if len(parts) == 4:
-            pr = parts[-1]  # must have len > 2
-            i = 1 if pr[1].isnumeric() else 2
-            pre_release = _PreRelease(pr[:i], int(pr[i:]))
-        else:
-            pre_release = None
+        segments = tuple(to_segment(s) for s in version.split('.'))
+        return cls(segments)
 
-        mmm = [int(i) for i in parts[:3]]
-        return cls(*mmm, pre_release)
-
+    def __str__(self) -> str:
+        return '.'.join(str(s) for s in self.segments)
 
 Version = _Version.from_(__version__)

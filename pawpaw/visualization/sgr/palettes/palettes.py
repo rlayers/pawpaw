7857991c4dd5de0d2beb4787eb0b6e519fa10c27
rlayers <114594097+rlayers@users.noreply.github.com>
2023-05-12 11:09:55 -0400

    Added "OLD_GLORY" palette

--- pawpaw/visualization/sgr/palettes/palettes.py
+++ pawpaw/visualization/sgr/palettes/palettes.py
@@ -1,6 +1,12 @@
 from pawpaw.visualization.sgr import C_PALETTE, Colors
 
 
+PAWPAW: C_OLD_GLORY = (
+    Colors.Rgb.from_24_bit(0xB31942),  # Red
+    Colors.Rgb.from_24_bit(0xFFFFFF),  # White
+    Colors.Rgb.from_24_bit(0x0A3161),  # Blue
+)
+
 PAWPAW: C_PALETTE = (
     Colors.Rgb.from_24_bit(0x533E30),
     Colors.Rgb.from_24_bit(0xD2AC70),

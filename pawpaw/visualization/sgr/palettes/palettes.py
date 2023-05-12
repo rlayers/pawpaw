97ef5865f48932b168da9790f47aa0d7197aca74
rlayers <114594097+rlayers@users.noreply.github.com>
2023-05-12 11:52:25 -0400

    Palette refresh

--- pawpaw/visualization/sgr/palettes/palettes.py
+++ pawpaw/visualization/sgr/palettes/palettes.py
@@ -1,6 +1,13 @@
 from pawpaw.visualization.sgr import C_PALETTE, Colors
 
 
+AIR_FORCE_ONE: C_PALETTE = (
+    Colors.Rgb.from_24_bit(0x3C79B4),  # Dark Blue
+    Colors.Rgb.from_24_bit(0xC9ECF5),  # Light Blue
+    Colors.Rgb.from_24_bit(0xF6F6F6),  # Off White
+    Colors.Rgb.from_24_bit(0xB7986C),  # Brown
+)
+
 OLD_GLORY: C_PALETTE = (
     Colors.Rgb.from_24_bit(0xB31942),  # Red
     Colors.Rgb.from_24_bit(0xFFFFFF),  # White
@@ -8,17 +15,22 @@ OLD_GLORY: C_PALETTE = (
 )
 
 PAWPAW: C_PALETTE = (
-    Colors.Rgb.from_24_bit(0x533E30),
-    Colors.Rgb.from_24_bit(0xD2AC70),
-    Colors.Rgb.from_24_bit(0xE4D1AE),
-    Colors.Rgb.from_24_bit(0x517D3D),
-    Colors.Rgb.from_24_bit(0x90C246),
+    Colors.Rgb.from_24_bit(0x533E30),  # Royal Brown
+    Colors.Rgb.from_24_bit(0xD2AC70),  # Light French Beige
+    Colors.Rgb.from_24_bit(0xE4D1AE),  # Desert Sand
+    Colors.Rgb.from_24_bit(0x517D3D),  # Fern Green
+    Colors.Rgb.from_24_bit(0x90C246),  # Android Green
 )
+"""
+  "Oriental Beauty"
+  https://www.schemecolor.com/oriental-beauty-color-combination.php
+"""
 
 TULIP_FIELD: C_PALETTE = (
-    Colors.Rgb.from_24_bit(0x779A36),
-    Colors.Rgb.from_24_bit(0xC73B4B),
-    Colors.Rgb.from_24_bit(0xEED257),
-    Colors.Rgb.from_24_bit(0xC95E9D),
-    Colors.Rgb.from_24_bit(0xF2672C),
+    Colors.Rgb.from_24_bit(0xFF6C98),  # Dark Pink
+    Colors.Rgb.from_24_bit(0xFEAA6D),  # Orange 
+    Colors.Rgb.from_24_bit(0xF7BACB),  # Light Pink
+    Colors.Rgb.from_24_bit(0xD879A2),  # Purple
+    Colors.Rgb.from_24_bit(0xF9E841),  # Yellow
+    Colors.Rgb.from_24_bit(0xE53F5D),  # Red
 )

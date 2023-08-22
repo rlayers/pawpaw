8cc895af24855b2d69705cff25ca55d31523ef39
rlayers <114594097+rlayers@users.noreply.github.com>
2023-08-22 10:54:20 -0400

    Adjust for future work

--- pawpaw/ontology/ontology.py
+++ pawpaw/ontology/ontology.py
@@ -5,70 +5,16 @@ import typing
 from pawpaw import Ito, Types
 from pawpaw.arborform import Itorator
 
-# {
-#     'vehicle':
-#     {
-#        'car':
-#        {
-#            'Ford': itor,
-#            'Chevy': itor,
-#        }
-#        'plane':
-#        {
-#            'Beechcraft': itor,
-#            'Cessna': itor,
-#        }
-#    }
-# }
-
-
-# r'\[vehicle/plane]|[vehicle/car/ford\]){1,3}
-# r'\[(?:vehicle/plane|vehicle/car/ford)\]{1,3}
-
-# r'\[(?:/vehicle/plane|ford)\]{1,3}  'startswith '/' -> root anchored; otherwise root or sub-path
-
-# r'\[(?:/vehicle/plane|ford/pinto)\]{1,3}
-
-
-
-# r'\[(?:/vehicle/plane|ford/pinto)\]{1,3}
-
-
-# full_path: /vehicle/car/ford/pinto/
-
-# Full anchors
-# /vehicle/car/ford/pinto/
-# /vehicle/car/ford/pinto/
-# r'(/vehicle/car/ford/pinto/)
-
-# Start anchor only
-# /vehicle/car:
-# /vehicle/car[/a/b/c...]/
-# r'(/vehicle/car   (?:/[^\)]+)*   /   )
- 
-# End anchor only
-# ford/pinto/:
-# /[a/b/c/...]ford/pinto/
-# r'(   /   ([^\)]+/)*   ford/pinto/   )
-
-
-# No anchors
-# ford/pinto:
-# /[a/b/c/...]ford/pinto[/a/b/c...]
-# r'(   /   ([^\)]+/)*   ford/pinto   (?:/[^\)]+)*   /   )
-
-
-
-# r'\(    (?:[^\)]+/)*   ford/pinto    (?:/[^\)]+)*    )
-
-# /vehicle/ford/pinto:
-# r'\(/vehicle/ford/pinto    (?:/[^\)]+)*     )
-
 C_RULE = Itorator | Types.F_ITO_2_IT_ITOS
 C_RULES = dict[str, C_RULE]
 C_PATH = typing.Sequence[str]
 
 class Discoveries(dict):
+    mathematical_white_square_brackets = {
+        'LEFT': '\u27E6',   # ⟦
+        'RIGHT': '\u27E7',  # ⟧
+    }
+
     def __init__(self, *args, **kwargs):
         self._itos: list[Ito] = list(kwargs.pop('itos', tuple()))
         dict.__init__(self, *args, **kwargs )
@@ -79,7 +25,15 @@ class Discoveries(dict):
     
     def __str__(self):
         c = ', '.join(f'{k}: {str(v)}' for k, v in self.items())
-        return f'{{itos: {[str(i) for i in self._itos]}, {c}}}'   
+        return f'{{itos: {[str(i) for i in self._itos]}, {c}}}'
+    
+    def transform(
+        self, 
+        entity_start_char: str = mathematical_white_square_brackets.LEFT,
+        entity_stop_char: str = mathematical_white_square_brackets.RIGHT
+    ) -> str:
+        raise NotImplemented()
+        
 
 class Ontology(dict):
     def __missing__(self, key):
@@ -127,6 +81,3 @@ class Ontology(dict):
             rv |= d
 
         return rv
-
-# MATHEMATICAL LEFT WHITE SQUARE BRACKET (U+27E6, Ps): ⟦
-# MATHEMATICAL RIGHT WHITE SQUARE BRACKET (U+27E7, Pe): ⟧

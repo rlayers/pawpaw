50953dfa7246747e0788dc15190a64ff3a5f0383
rlayers <114594097+rlayers@users.noreply.github.com>
2024-01-11 16:00:06 -0500

    Table refactoring

--- pawpaw/nlp/table.py
+++ pawpaw/nlp/table.py
@@ -3,7 +3,6 @@ import dataclasses
 
 import regex
 import pawpaw
-from pawpaw import nuco
 
 
 @dataclasses.dataclass(frozen=True)
@@ -19,8 +18,9 @@ class Table(ABC):
     def re(self) -> regex.Pattern:
         ...
 
+    @abstractmethod
     def get_itor(self) -> pawpaw.arborform.Itorator:
-        return pawpaw.arborform.Extract(self.re, tag=self.itor_tag)
+        ...
 
 
 class StyledTable(Table):
@@ -39,168 +39,3 @@ class StyledTable(Table):
 
     def get_itor(self) -> pawpaw.arborform.Itorator:
         return pawpaw.arborform.Extract(self._re, tag=self.tag)
-
-
-class Tables:
-    """
-    Notes:
-
-    MUST HAVE CHARACTERISTICS:
-
-        - Must be able to determine start and stop in order to identify within larger
-          unstructured text
-
-        - Have way to distinguish columns and ROWS (i.e., a table represented with tabs
-          doesn't allow for row delineations
-
-        - Optionally has a header row(s)
-
-    STYLES:
-
-    Style 1: md style
-
-        | A | B | C |
-        |---|:-:|--:|
-        | a | b | c |
-        | d | e | f |
-
-    Style 2: rst
-    
-        2.a rst Simple Table
-
-        =====  =====  =======
-        A      B      A and B
-        =====  =====  =======
-        False  False  False
-        True   False  False
-        False  True   False
-        True   True   True
-        =====  =====  =======
-
-        2.b rst Grid Table
-
-        +------------+------------+-----------+
-        | Header 1   | Header 2   | Header 3  |
-        +============+============+===========+
-        | body row 1 | column 2   | column 3  |
-        +------------+------------+-----------+
-        | body row 2 | Cells may span columns.|
-        +------------+------------+-----------+
-        | body row 3 | Cells may  | - Cells   |
-        +------------+ span rows. | - contain |
-        | body row 4 |            | - blocks. |
-        +------------+------------+-----------+
-
-    Style 3: Ascii doc
-
-        [cols="e,m,^,>s",width="25%"]
-        |============================
-        |1 >s|2 |3 |4
-        ^|5 2.2+^.^|6 .3+<.>m|7
-        ^|8
-        |9 2+>|10
-        |============================
-
-    Style 4: Misc ASCII
-
-        pipe, hypen, plus
-        
-        +---+---+---+
-        | A | B | C |
-        +---+---+---+
-        | a | b | c |
-        +---+---+---+
-        | d | e | f |
-        +---+---+---+
-
-        pipe, em-dash, plus
-
-        +———+———+———+
-        | A | B | C |
-        +———+———+———+
-        | a | b | c |
-        +———+———+———+
-        | d | e | f |
-        +———+———+———+
-
-        misc ascii box drawing line styles
-        
-        ┌───┬───┬───┐
-        │ A │ B │ C │
-        ├───┼───┼───┤
-        │ a │ b │ c │
-        ├───┼───┼───┤
-        │ d │ e │ f │
-        └───┴───┴───┘    
-
-        ┏━━━┳━━━┳━━━┓
-        ┃ A ┃ B ┃ C ┃
-        ┣━━━╋━━━╋━━━┫
-        ┃ a ┃ b ┃ c ┃
-        ┣━━━╋━━━╋━━━┫
-        ┃ d ┃ e ┃ f ┃
-        ┗━━━┻━━━┻━━━┛
-
-        ┏━━━┳━━━┳━━━┓
-        ┃ A ┃ B ┃ C ┃
-        ┡━━━╇━━━╇━━━┩
-        │ a │ b │ c │
-        ├───┼───┼───┤
-        │ d │ e │ f │
-        └───┴───┴───┘
-            
-        ╔═══╦═══╦═══╗
-        ║ A ║ B ║ C ║
-        ╠═══╬═══╬═══╣
-        ║ a ║ b ║ c ║
-        ╠═══╬═══╬═══╣
-        ║ d ║ e ║ f ║
-        ╚═══╩═══╩═══╝    
-
-        ╔═══╤═══╤═══╗
-        ║ A │ B │ C ║
-        ╟───┼───┼───╢
-        ║ a │ b │ c ║
-        ╟───┼───┼───╢
-        ║ d │ e │ f ║
-        ╚═══╧═══╧═══╝    
-    
-    Style 5 [Unamed]
-
-        5.a
-
-        -----+-----+-----
-        A  |  B  |  C
-        -----+-----+-----      
-        aaa | bbb | ccc
-        -----+-----+-----      
-
-        5.b
-
-        -------------------
-        |  A  |  B  |  C  |
-        -------------------     
-        | aaa | bbb | ccc |
-        -------------------     
-
-    """
-
-    p = r'-{2,}[\-\+]*-{2,}'
-    style = TableStyle(p, p, p, p)
-    Style_A = StyledTable(style, 'table type A extractor')
-    del style
-    del p
-
-    '''
-        -------------
-        | A | B | C |
-        -------------
-        | a | b | c |
-        | a |   | c
-        -------------       
-    '''
-    p = r'-{2,}'
-    style = TableStyle(p, p, p, p)
-    Style_B = StyledTable(style, 'table type A extractor')
-    del style
-    del p

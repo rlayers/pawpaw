84ab231b7edd00b3de64b90b97cfef285003fc81
rlayers <a.nov.guy@gmail.com>
2025-03-24 23:29:38 -0400

    version inc

--- pawpaw/_version.py
+++ pawpaw/_version.py
@@ -3,7 +3,7 @@ import typing
 
 import regex
 
-__version__ = '1.0.0rc8'
+__version__ = '1.0.1'
 """The str literal that build, setup, documentation, and other tools typically want."""
 
 class Version:
